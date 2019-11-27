54e566eb506c2478c7eaec64785277c47d12a22f
Эркин Камилов <erkimkamilov@gmail.com>
2019-11-27 20:43:48 +0500

    Code cleaning.

--- Classes/GOSTCryptographer.cs
+++ Classes/GOSTCryptographer.cs
@@ -9,14 +9,7 @@ namespace lab1_Encryption_.Classes
 {
     public class GOSTCryptographer : ICryptographer
     {
-        public static readonly byte[] IP ={ 57, 49, 41, 33, 25, 17, 9, 1, 58, 50,
-                                             42, 34, 26, 18, 10, 2, 59, 51, 43, 35,
-                                             27, 19, 11, 3, 60, 52, 44, 36, 63, 55,
-                                             47, 39, 31, 23, 15, 7, 62, 54, 46, 38,
-                                             30, 22, 14, 6, 61, 53, 45, 37, 29, 21,
-                                             13, 5, 28, 20, 12, 4 };
-
-        public static readonly byte[,] S1 ={ { 14, 4, 13, 1, 2, 15, 11, 8, 3, 10, 6, 12, 5, 9,
+        public static readonly byte[,] S ={ { 14, 4, 13, 1, 2, 15, 11, 8, 3, 10, 6, 12, 5, 9,
                                                0, 7 },
                                              { 0, 15, 7, 4, 14, 2, 13, 1, 10, 6, 12, 11, 9,
                                                5, 3, 8 },
@@ -29,9 +22,8 @@ namespace lab1_Encryption_.Classes
                                              { 13, 11, 4, 1, 3, 15, 5, 9, 0, 10, 14, 7, 6, 8, 2, 12},
                                              { 1, 15, 13, 0, 5, 7, 10, 4, 9, 2, 3, 14, 6, 11, 8, 12 } };
 
-        public static readonly byte[,] S0 = new byte[8, 16];
-
-        public static readonly byte[,] S = new byte[8, 16] { 
+        // for debug:
+        public static readonly byte[,] S1 = new byte[8, 16] {
             { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 },
             { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 },
             { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 },
@@ -40,17 +32,6 @@ namespace lab1_Encryption_.Classes
             { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 },
             { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 },
             { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 },
-        };
-
-        public static readonly byte[] E =
-        {
-            32,1,2,3,4,5,
-            4,5,6,7,8,9,
-            8,9,10,11,12,13,
-            12,13,14,15,16,17,
-            16,17,18,19,20,21,
-            24,25,26,27,28,29,
-            28,29,30,31,32,1
         };
 
         public GOSTCryptographer(string key)
@@ -58,14 +39,14 @@ namespace lab1_Encryption_.Classes
             Key = key;
         }
 
-        protected UInt32[] _key;
+        protected UInt32[] _keys;
         public string Key
         {
             set
             {
                 //_key = new BitArray(Encoding.ASCII.GetBytes(value));
                 //_key.Length = 256;
-                _key = new UInt32[8];
+                _keys = new UInt32[8];
 
                 var byteKey = new byte[32];
 
@@ -80,13 +61,13 @@ namespace lab1_Encryption_.Classes
                     k = k | byteKey[i + 2];
                     k = k << 8;
                     k = k | byteKey[i + 3];
-                    _key[i / 4] = k;
+                    _keys[i / 4] = k;
                 }
             }
         }
         public byte[] Encrypt(byte[] data)
         {
-            UInt64[] inputBlocks =  GetBlocks(data);
+            UInt64[] inputBlocks = GetBlocks(data);
             var outputBlocks = SimpleReplacement(inputBlocks, true);
 
             byte[] outputBytes = GetBytes(outputBlocks);
@@ -103,7 +84,7 @@ namespace lab1_Encryption_.Classes
         {
             var inputBytes = input;
             // Проверка открытого текста на кратность 64 битам:
-            int blocksCount = inputBytes.Length/8;
+            int blocksCount = inputBytes.Length / 8;
             var mod = inputBytes.Length % 8;
             if (mod != 0)
             {
@@ -121,7 +102,7 @@ namespace lab1_Encryption_.Classes
                 Blocks[i] = GetUint64From8Bytes(inputBytesBlocks, 8 * i);
             }
 
-            
+
             return Blocks;      // check return
         }
 
@@ -129,7 +110,7 @@ namespace lab1_Encryption_.Classes
         {
             byte[] bytes = new byte[blocks.Length * 8];
 
-            for (int i = 0; i < blocks.Length; i ++)
+            for (int i = 0; i < blocks.Length; i++)
             {
                 var temp = Get8BytesFromUInt64(blocks[i]);
 
@@ -167,7 +148,7 @@ namespace lab1_Encryption_.Classes
         {
             var result = new UInt64[inputBlocks.Length];
 
-            var cycleKeys = GenerateCycleKeys(_key);
+            var cycleKeys = GenerateCycleKeys(_keys);
             // Reverse key on Decryption:
             if (!isEncryption)
             {
@@ -185,23 +166,10 @@ namespace lab1_Encryption_.Classes
         {
             UInt64 result = block;
 
-            foreach(var key in cycleKeys)
+            foreach (var key in cycleKeys)
             {
                 result = MainCryptoStep(result, key);
             }
-            /*
-            for (int i = 0; i < 3; i++)
-            {
-                for (int j = 0; j < 8; j++)
-                {
-                    result = this.MainCryptoStep(result, key[j]);
-                }
-            }
-
-            for (int j = 7; j >= 0; j--)
-            {
-                result = this.MainCryptoStep(result, key[j]);
-            }*/
 
             result = ((result & UInt32.MaxValue) << 32) | (result >> 32);
 
@@ -230,7 +198,7 @@ namespace lab1_Encryption_.Classes
             #endregion
 
             #region шаг 3 - сдвиг влево на 11
-            
+
             uint step3Value = BitShift(step2Value, -11);
 
             #endregion
@@ -267,7 +235,7 @@ namespace lab1_Encryption_.Classes
                 step1Value = step1Value & (UInt32.MaxValue - ((UInt32)0xf << shift));
                 result += S[7 - i, index];
             }
-            return result;      
+            return result;
         }
         protected UInt32 BitShift(UInt32 block, int bias)
         {
@@ -302,49 +270,6 @@ namespace lab1_Encryption_.Classes
 
             return newBlock;        // check return.
         }
-
-        protected BitArray Calculate(BitArray input, bool isEncryption)
-        {
-            var X = GenerateSubkeys(isEncryption);
-            var outputBits = new BitArray(0);
-            // Проверка открытого текста на кратность 64 битам:
-            var mod = input.Length % 64;
-            if (mod != 0)
-            {
-                input.Length += 64 - mod;
-            }
-
-            int numberOfBlocks = (input.Length / 64);
-
-            for (int block = 0; block < numberOfBlocks; block++)
-            {
-                var A = new BitArray(32);
-                var B = new BitArray(32);
-
-                // Initializating inputs:
-                for (int i = 0; i < 32; i++)
-                {
-                    A[i] = input[i + 64 * block];
-                    B[i] = input[i + 64 * block + 32];
-                }
-
-                for (int round = 0; round < 32; round++)
-                {
-                    var f = Function(A, X[round], round);
-                    var newA = B;
-                    newA.Xor(f);
-                    var newB = A;
-
-                    A = newA;
-                    B = newB;
-                }
-                outputBits = BitsAppend(outputBits, A);
-                outputBits = BitsAppend(outputBits, B);
-            }
-
-            return outputBits;
-        }
-
         private UInt32[] GenerateCycleKeys(UInt32[] keys)
         {
             UInt32[] CycleKeys = new UInt32[32];
@@ -353,169 +278,16 @@ namespace lab1_Encryption_.Classes
             {
                 for (int j = 0; j < 8; j++)
                 {
-                    CycleKeys[j] = _key[j];
+                    CycleKeys[j] = _keys[j];
                 }
             }
 
             for (int j = 7; j >= 0; j--)
             {
-                CycleKeys[j] = _key[j];
+                CycleKeys[j] = _keys[j];
             }
 
             return CycleKeys;
-        }
-
-        protected BitArray Function(in BitArray A, in BitArray X, int round)
-        {
-            var newA = new BitArray(A);
-            newA.Xor(X);
-            var partsA = new BitArray[8];
-            var outputBits = new BitArray(0);
-
-            // DEBUG:
-            byte[] bytes = new byte[newA.Count];
-            newA.CopyTo(bytes, 0);
-            ////////
-
-            for (int i = 0; i < partsA.Length; i++)
-            {
-                partsA[i] = new BitArray(newA.Cast<bool>().Skip(i*4).Take(4).ToArray());
-            }
-            for (int i = 0; i < partsA.Length; i++)
-            {
-                var val = new byte[1];
-                partsA[i].CopyTo(val, 0);
-                byte[] newVal = new byte[] { S[i, val[0]] };
-
-                partsA[i] = new BitArray(newVal);
-                // From 8 bits to 4 bits:
-                partsA[i] = new BitArray(partsA[i].Cast<bool>().Take(4).ToArray());
-                
-            }
-            
-            foreach (var part in partsA)
-            {
-                outputBits = BitsAppend(outputBits, part);
-            }
-
-            var output = BitShift(outputBits, 11);
-
-            return output;
-        }
-
-        protected BitArray[] GenerateSubkeys(bool isEncryption)
-        {
-            var K = new BitArray[8];
-            var X = new BitArray[32];
-            int i, j;
-
-            // K initialization:
-            for (i = 0; i < K.Length; i++)
-            {
-                K[i] = new BitArray(32);
-
-                for (j = 0; j < K[i].Length; j++)
-                {
-                    //K[i][j] = _key[(_key.Length / K.Length) * i + j];
-                }
-            }
-
-            if (isEncryption)
-            {
-                // X initialization: 
-                j = 0;
-                for (i = 0; i < 24; i++)
-                {
-                    X[i] = K[j];
-                    j++;
-                    if (j == 8) j = 0;
-                }
-
-                // Reversed X initialization:
-                j = 7;
-                for (i = 24; i < 32; i++)
-                {
-                    X[i] = K[j];
-                    j--;
-                }
-                return X;
-            }
-            else
-            {
-                // X initialization: 
-                j = 0;
-                for (i = 0; i < 8; i++)
-                {
-                    X[i] = K[j];
-                    j++;
-                }
-
-                // Reversed X initialization:
-                j = 7;
-                for (i = 8; i < 32; i++)
-                {
-                    X[i] = K[j];
-                    j--;
-                    if (j == -1) j = 7;
-                }
-                return X;
-            }
-            
-        }
-
-        public BitArray BitsAppend(BitArray current, BitArray after)
-        {
-            var bools = new bool[current.Count + after.Count];
-            current.CopyTo(bools, 0);
-            after.CopyTo(bools, current.Count);
-            return new BitArray(bools);
-        }
-        public void BitsReverse(BitArray array)
-        {
-            int length = array.Length;
-            int mid = (length / 2);
-
-            for (int i = 0; i < mid; i++)
-            {
-                bool bit = array[i];
-                array[i] = array[length - i - 1];
-                array[length - i - 1] = bit;
-            }
-        }
-        protected BitArray BitShift(BitArray bitArray, int bias)
-        {
-            //int bitsInChar = sizeof(char) * 8;
-            BitArray newBitArray = new BitArray(bitArray.Length);
-
-            if (bias >= 0)
-            {
-                // Смещение вправо:
-                for (int i = 0; i < bitArray.Length - bias; i++)
-                {
-                    newBitArray[i + bias] = bitArray[i];
-                }
-                // Перенос битов справа налево:
-                for (int i = bitArray.Length - bias; i < bitArray.Length; i++)
-                {
-                    newBitArray[bitArray.Length - i - 1] = bitArray[i];
-                }
-            }
-            else
-            {
-                bias = -bias;
-                // Смещение влево:
-                for (int i = 0; i < bitArray.Length - bias; i++)
-                {
-                    newBitArray[i] = bitArray[i + bias];
-                }
-                // Перенос битов:
-                for (int i = bitArray.Length - bias; i < bitArray.Length; i++)
-                {
-                    newBitArray[i] = bitArray[bitArray.Length - i - 1];
-                }
-            }
-
-            return newBitArray;
         }
     }
 }
